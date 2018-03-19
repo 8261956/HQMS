0689bcd83f7a7962ac290d803730d32326331a74
fanchunke <fanchunke1991@163.com>
2018-03-19 11:41:02 +0800

    fix: [瑞慈]修改接口地址，并修改测试文件

--- HQueue/project/ruici/station_printer_test/station_printer.py
+++ HQueue/project/ruici/station_printer_test/station_printer.py
@@ -13,6 +13,7 @@ _config = ConfigParser.ConfigParser()
 _config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 stationID = int(_config.get("station", "stationID"))
 server = _config.get("station", "server")
+server_url = _config.get("station", "server_url")
 
 
 class StationPrinter(object):
@@ -24,13 +25,19 @@ class StationPrinter(object):
         key = "print_stationID_{0}".format(self.stationID)
 
         while True:
-            queue = CachedGetValue(json.dumps(key))
-            while len(queue):
-                result = queue.popleft()
-                CahedSetValue(json.dumps(key), queue, timeout=0)
-                current_time = datetime.datetime.now()
-                print "[{0}] new visitor: {1}".format(current_time, result)
-            time.sleep(2)
+            # queue = CachedGetValue(json.dumps(key))
+            # while queue and len(queue):
+            #     result = queue.popleft()
+            #     CahedSetValue(json.dumps(key), queue, timeout=300)
+            #     current_time = datetime.datetime.now()
+            #     print "[{0}] new visitor: {1}".format(current_time, result)
+            # time.sleep(2)
+            import requests
+            data = {"action": "getNextVisitor", "stationID": self.stationID}
+            url = os.path.join(server_url, "hqueue/main/stationPrinter")
+            result = requests.post(url, json.dumps(data))
+            print result.json()
+            time.sleep(1)
 
 
 if __name__ == '__main__':
