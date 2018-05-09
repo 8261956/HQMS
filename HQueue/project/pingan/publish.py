11bc26cf8ad1de024076d9b229916b06d76ba64c
qiu <514473821@qq.com>
2018-05-09 09:28:58 +0800

    打印发布服务

--- HQueue/project/pingan/publish.py
+++ HQueue/project/pingan/publish.py
@@ -9,6 +9,14 @@ localPort = "1883"
 localTopic = "station_printer"
 
 def publish_printer(vInfo):
-    publish.single(localTopic, payload=vInfo, qos=0, retain=False, hostname="localhost",
-           port=1883, client_id="", keepalive=60, will=None, auth=None, tls=None,
+    printInfo = {
+        "cmd": "print",
+        "val": {
+            "print_name": vInfo.get("name",""),
+            "print_snumber" : vInfo.get("snumber",""),
+            "print_queue": vInfo.get("queue", "")
+        }
+    }
+    publish.single(localTopic, payload=json.dumps(printInfo), qos=0, retain=False, hostname = localServer,
+           port=localPort, client_id="", keepalive=60, will=None, auth=None, tls=None,
            protocol=mqtt.MQTTv311)
