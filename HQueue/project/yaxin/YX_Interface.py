589eb646744ccdf09e8392cf53a14d69e58573ef
qiu <514473821@qq.com>
2018-05-18 16:15:45 +0800

    ESB Test

--- HQueue/project/yaxin/YX_Interface.py
+++ HQueue/project/yaxin/YX_Interface.py
@@ -22,10 +22,12 @@ TYPE_FAYAO = "01"
 TYPE_YIJI = "02"
 TYPE_ZHUYUAN = "03"
 
-global tClient
-tClient = None
-
 def AddSoapHeader_ESB(client):
+    """
+    为客户端发送请求时添加ESB报头
+    :param client: 客户端
+    :return: None
+    """
     from suds.sax.element import Element
     baseElement = Element('head')
     baseElement.append(Element("context").setText("HISWebService"))
@@ -39,11 +41,9 @@ def AddSoapHeader_ESB(client):
 
 def getClient():
     print "getWebService func In"
-    global tClient
-    if tClient == None:
-        tClient = Client(YX_url)
-        AddSoapHeader_ESB(tClient)
-        print "Client Init ok"
+    tClient = Client(YX_url)
+    AddSoapHeader_ESB(tClient)
+    print "Client Init ok"
     return tClient
 
 def MainMethod(SourceName , OpNum , OpControl ,OpData , Picture = None):
@@ -468,6 +468,42 @@ class YXSourceController:
         fydl = data.get("fydl","")
         httpPostRegist(blh, ywlx, dldm,fydl)
         return {"result" : "success"}
+
+import httplib
+
+def mdsmssend(sn, pwd, mobile, context):
+    # 定义发送报文
+    SoapMessage = '''<?xml version="1.0" encoding="utf-8"?>
+        <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+          <soap:Body>
+            <mdsmssend xmlns="http://entinfo.cn/">
+              <sn>%s</sn>
+              <pwd>%s</pwd>
+              <mobile>%s</mobile>
+              <content>%s</content>
+            </mdsmssend>
+          </soap:Body>
+        </soap:Envelope>'''
+
+    SoapMessage = SoapMessage % (sn, pwd, mobile, context)
+    # 使用的WebService地址为sdk.entinfo.cn:8061/webservice.asmx，
+    webservice = httplib.HTTP("http://133.0.14.140:8280/main/queue")
+    # 连接到服务器后的第一个调用。它发送由request字符串到到服务器
+    webservice.putrequest("POST", "/webservice.asmx")
+    webservice.putheader("Host", "http://133.0.14.140:8280/main/queue")
+    webservice.putheader("User-Agent",
+                         "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)")
+    webservice.putheader("Content-type", "text/xml; charset=\"UTF-8\"")
+    webservice.putheader("Content-length", "%d" % len(SoapMessage))
+    webservice.putheader("SOAPAction", "\"http://entinfo.cn/mdsmssend\"")
+    # 发送空行到服务器，指示header的结束
+    webservice.endheaders()
+    # 发送报文数据到服务器
+    webservice.send(SoapMessage)
+    # 获取返回HTTP 响应信息
+    statuscode, statusmessage, header = webservice.getreply()
+    return statuscode
+
 
 #print "-----InqOnDuty : ------"
 #InqOnDuty("2017.06.01","2017.06.19")
